2651f1817edcc5adca77a8152ea69652d0d09a66
Александр Озеров <cdg.axel@gmail.com>
2023-01-30 00:15:57 +0300

    daily

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 #include "stepik.h"
 
 using namespace std;
 
 int main() {
-    for (;0;);
-    cout << endl;
+    int n, m, i, j, k, cnt, found = 0;
+    cin >> n >> m;
+    vector <vector <int>> a(n, vector<int> (m));
+
+    for (i = 0; i < n; ++i)
+        for (j = 0; j < m; ++j) cin >> a[i][j];
+    cin >> k;
+
+    for (i = 0; i < n && !found; ++i)
+        for (j = cnt = 0; j < m && !found; ++j)
+            if (a[i][j]) cnt = 0;
+            else if (++cnt >= k) found = i + 1;
+
+    cout << found << endl;
+    return 0;
 }
 
